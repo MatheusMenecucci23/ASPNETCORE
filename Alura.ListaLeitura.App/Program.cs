692bfca5c9536530c8c79639d75310fa19f3bee8
matheus.menecucci@domvsit.com.br <matheus.menecucci@domvsit.com.br>
2021-09-30 17:45:01 -0300

    Adicionando projeto Adicionar arquivos de projeto.

---
+++ Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            //Criando um hospedeiro para rodar a aplicação na web
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            //Rodando os servidor
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+
+
+    
+}
